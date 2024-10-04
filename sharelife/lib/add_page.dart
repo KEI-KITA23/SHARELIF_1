8e96334e1ae3320722468dfadf55dadbe4fa9364
Sota109 <sotaawareminorei1009@icloud.com>
2024-10-04 19:00:18 +0900

    serch

--- sharelife/lib/add_page.dart
+++ sharelife/lib/add_page.dart
@@ -1,10 +1,122 @@
+// import 'package:flutter/material.dart';
+// import 'package:sharelife/main.dart';
+// import 'package:sharelife/my_page.dart';
+// import 'package:sharelife/ranking_page.dart';
+
+// class AddPage extends StatelessWidget {
+//   const AddPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sharelife'),
+//       ),
+//       body: Column(
+//         children: [
+//           const Row(
+//             mainAxisAlignment: MainAxisAlignment.center, // 水平方向に中央揃え
+//             crossAxisAlignment: CrossAxisAlignment.center, // 縦方向に中央揃え
+//           ),
+//           Expanded(
+//             child: Center(
+//               child: Container(
+//                 width: 500, // 幅を指定してサイズを調整
+//               ),
+//             ),
+//           ),
+//           Center(
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     Navigator.pushReplacement(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => FirstPage()),
+//                     );
+//                   },
+//                   child: Icon(
+//                     Icons.home, // homeのアイコン
+//                     color: const Color.fromARGB(255, 106, 120, 115), // アイコンの色
+//                     size: 55.0, // アイコンのサイズ
+//                   ),
+//                   style: ElevatedButton.styleFrom(
+//                       backgroundColor: Colors.transparent,
+//                       shadowColor: Colors.transparent,
+//                       elevation: 0,
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(0),
+//                       )),
+//                 ),
+//                 SizedBox(
+//                   width: 20,
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     Navigator.pushReplacement(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => RankingPage()),
+//                     );
+//                   },
+//                   child: Icon(
+//                     Icons.local_fire_department, // ランキングのアイコン
+//                     color: const Color.fromARGB(255, 106, 120, 115), // アイコンの色
+//                     size: 55.0, // アイコンのサイズ
+//                   ),
+//                   style: ElevatedButton.styleFrom(
+//                       backgroundColor: Colors.transparent,
+//                       shadowColor: Colors.transparent,
+//                       elevation: 0,
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(0),
+//                       )),
+//                 ),
+//                 SizedBox(
+//                   width: 20,
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     Navigator.pushReplacement(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => MyPage()),
+//                     );
+//                   },
+//                   child: Icon(
+//                     Icons.person, // 人のアイコン
+//                     color: const Color.fromARGB(255, 106, 120, 115), // アイコンの色
+//                     size: 55.0, // アイコンのサイズ
+//                   ),
+//                   style: ElevatedButton.styleFrom(
+//                       backgroundColor: Colors.transparent,
+//                       shadowColor: Colors.transparent,
+//                       elevation: 0,
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(0),
+//                       )),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:sharelife/main.dart';
 import 'package:sharelife/my_page.dart';
-import 'package:sharelife/ranking_page.dart';
 
-class AddPage extends StatelessWidget {
+class AddPage extends StatefulWidget {
   const AddPage({super.key});
+
+  @override
+  _AddPageState createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  String _searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +126,23 @@ class AddPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center, // 水平方向に中央揃え
-            crossAxisAlignment: CrossAxisAlignment.center, // 縦方向に中央揃え
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: '検索',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (query) {
+                setState(() {
+                  _searchQuery = query;
+                });
+              },
+            ),
           ),
           Expanded(
             child: Center(
-              child: Container(
-                width: 500, // 幅を指定してサイズを調整
-              ),
+              child: Text('検索結果: $_searchQuery'),
             ),
           ),
           Center(
@@ -37,9 +157,9 @@ class AddPage extends StatelessWidget {
                     );
                   },
                   child: Icon(
-                    Icons.home, // homeのアイコン
-                    color: const Color.fromARGB(255, 106, 120, 115), // アイコンの色
-                    size: 55.0, // アイコンのサイズ
+                    Icons.home,
+                    color: const Color.fromARGB(255, 106, 120, 115),
+                    size: 55.0,
                   ),
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.transparent,
@@ -53,16 +173,11 @@ class AddPage extends StatelessWidget {
                   width: 20,
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => RankingPage()),
-                    );
-                  },
+                  onPressed: () {},
                   child: Icon(
-                    Icons.local_fire_department, // ランキングのアイコン
-                    color: const Color.fromARGB(255, 106, 120, 115), // アイコンの色
-                    size: 55.0, // アイコンのサイズ
+                    Icons.local_fire_department,
+                    color: const Color.fromARGB(255, 106, 120, 115),
+                    size: 55.0,
                   ),
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.transparent,
@@ -83,9 +198,9 @@ class AddPage extends StatelessWidget {
                     );
                   },
                   child: Icon(
-                    Icons.person, // 人のアイコン
-                    color: const Color.fromARGB(255, 106, 120, 115), // アイコンの色
-                    size: 55.0, // アイコンのサイズ
+                    Icons.person,
+                    color: const Color.fromARGB(255, 106, 120, 115),
+                    size: 55.0,
                   ),
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.transparent,
